69bd5ae94fdcf646e6d7a3603faa6b9d86781877
manyu1028 <13291260792@163.com>
2016-11-26 21:01:44 +0800

    Finish  static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,11 +6,21 @@ describe "Static pages"  do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
 		end
+
+		it "should have the right title" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+		end
 	end
 	describe "Help page" do
 		it "should have the content 'Help'" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
 		end
 	end
 	describe "About page" do
@@ -18,5 +28,12 @@ describe "Static pages"  do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
 		end
+
+		it "should have the right title" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+		end
 	end
+
+
 end
